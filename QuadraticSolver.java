eeed6945d28255978bbc68cd47732df57c127698
Vansh Saboo <58910619+49PES@users.noreply.github.com>
2021-11-21 00:47:48 -0500

    Update QuadraticSolver.java

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -1,49 +1,58 @@
 public class QuadraticSolver{
 	public static String quadSolver(int[] coefficients){
-	    int a = coefficients[0]; int b = coefficients[1]; int c = coefficients[2];
-	    double posTerm, negTerm;
-	    
-	    int discriminant = (int) Math.pow(b, 2) - 4 * a * c;
-	    
-	    if(discriminant > 0){
-	        posTerm = (-b + Math.sqrt(discriminant))/(2 * a);
-	        if((double) (int) posTerm == posTerm){
-	            negTerm = (-b - Math.sqrt(discriminant))/(2 * a);  
-	            return (String.valueOf((int) posTerm) + ", " + String.valueOf((int) negTerm));} 
-	       else{
-	            int maxSquare = maxSquareFactor(discriminant);
-	            discriminant = discriminant/((int) Math.pow(maxSquare, 2));
-	            int gcd = gcdER(gcdER(b, maxSquare), 2 * a);
-	            
-	            if(maxSquare == 1){
-	                return "(" + -b + " + √" + discriminant + ")/" + String.valueOf(2 * a) + 
-	                ", (" + -b + " - √" + discriminant + ")/" + String.valueOf(2 * a);}
-	                
-                if(gcd == 2 * a){
-                    return String.valueOf(-b/gcd) + " + " + String.valueOf(maxSquare/gcd) + "√" + discriminant + 
-	                ", " + String.valueOf(-b/gcd) + " - " + String.valueOf(maxSquare/gcd) + "√" + discriminant;
+    int a = coefficients[0]; int b = coefficients[1]; int c = coefficients[2];
+    double posTerm, negTerm;
+    
+    int discriminant = (int) Math.pow(b, 2) - 4 * a * c;
+    
+    if(discriminant >= 0){
+        posTerm = (-b + Math.sqrt(discriminant))/(2 * a);
+        if((double) (int) posTerm == posTerm){
+            negTerm = (-b - Math.sqrt(discriminant))/(2 * a);  
+            return String.format("%d, %d", (int) posTerm, (int) negTerm);}  
+       else{
+            int maxSquare = maxSquareFactor(discriminant);
+            discriminant = discriminant/((int) Math.pow(maxSquare, 2));
+            int gcd = gcdER(gcdER(b, maxSquare), 2 * a);
+            if(discriminant == 1){
+                return String.format("%1$d/%3$d, %2$d/%3$d", (-b + maxSquare)/gcd, (-b - maxSquare)/gcd, (2 * a)/gcd);
+            }
+            if(maxSquare == 1){
+                return String.format("(%1$d + √%2$d)/%3$d, (%1$d - √%2$d)/%3$d", -b, discriminant, 2 * a); 
+            }
+             if(gcd == 2 * a){
+                return String.format("%1$d + %2$d√%3$d, %1$d - %2$d√%3$d", -b/gcd, maxSquare/gcd, discriminant);
+            }
+            if(-gcd == 2 * a){
+                return String.format("%1$d + %2$d√%3$d, %1$d - %2$d√%3$d", b/gcd, maxSquare/gcd, discriminant);
+            } 
+            
+            if(gcd > 1){
+                if(maxSquare/gcd != 1){
+                    return String.format("(%1$d + %2$d√%3$d)/%4$d, (%1$d - %2$d√%3$d)/%4$d", -b/gcd, maxSquare/gcd, discriminant, (2 * a)/gcd);
                 }
-                
-                if(gcd == -2 * a){
-                    return String.valueOf(b/gcd) + " + " + String.valueOf(maxSquare/gcd) + "√" + discriminant + 
-	                ", " + String.valueOf(b/gcd) + " - " + String.valueOf(maxSquare/gcd) + "√" + discriminant;
+                else{
+                    return String.format("(%1$d + √%2$d)/%3$d, (%1$d - √%2$d)/%3$d", -b/gcd, discriminant, (2 * a)/gcd);
                 }
-                
-                if(gcd > 1){
-                    if(maxSquare/gcd != 1){
-                        return "(" + String.valueOf(-b/gcd) + " + " + String.valueOf(maxSquare/gcd) + "√" + discriminant + ")/" + String.valueOf((2 * a)/gcd) + 
-                            ", (" + String.valueOf(-b/gcd) + " + " + String.valueOf(maxSquare/gcd) + "√" + discriminant + ")/" + String.valueOf((2 * a)/gcd);}
-                    else{
-                        return "(" + String.valueOf(-b/gcd) + " + √" + discriminant + ")/" + String.valueOf((2 * a)/gcd) + 
-                            ", (" + String.valueOf(-b/gcd) + " + √" + discriminant + ")/" + String.valueOf((2 * a)/gcd);}
-                    }        
-                } 	            
-	        }
-	    
-	    
-	    return "";
-	
+                }        
+            } 	            
+        }
+    
+    else{
+        discriminant *= -1;
+        int maxSquare = maxSquareFactor(discriminant);
+        int gcd = gcdER(gcdER(-b, maxSquare), 2 * a);
+        if(gcd == 2 * a){
+            if(discriminant == 1){return String.format("%1$d + i%2$d, %1$d - i%2$d", -b/gcd, maxSquare/gcd);}
+        }
+        /* if(discriminant != 1){
+            return String.format("");
+        } */
+    }
+    return "";
+
 	}
+	
 	  public static int gcdER( int a, int b ) {
         a = Math.abs(a);
         b = Math.abs(b);
@@ -60,18 +69,19 @@ public class QuadraticSolver{
 
 	public static int maxSquareFactor(int val){
 	    int max = 1;
-	    for(int i = 1; i <= Math.sqrt(val); i++){
-	        if(val/(Math.pow(i, 2)) % 1.0 == 0.0){max = i;}
+	    int num = val;
+	    for(int i = 2; i <= num; i++){
+	        while(num % Math.pow(i, 2) == 0){
+	            max *= i;
+	            num /= Math.pow(i, 2);
+	        }
 	    }
-	    return max;
+	   return max;
 	}
+	
 	public static void main(String[] args) {
+	    int[] strawberry = {1, -10, 13};
 		System.out.println("Hello World");
-		int[] quad1 = {1, 6, 5};
-		int[] quad2 = {1, -6, 5};
-		int[] quad3 = {7, -100, 59};
-		System.out.println(quadSolver(quad1));
-		System.out.println(quadSolver(quad2));
-		System.out.println(quadSolver(quad3));
+		System.out.println(quadSolver(strawberry));
 	}
 }
