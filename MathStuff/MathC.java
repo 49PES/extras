aa5e8f73cc9e85d57bca86e790dd4d4f0e56bebf
Vansh Saboo <58910619+49PES@users.noreply.github.com>
2021-12-28 18:34:39 -0500

    Update MathC.java

--- MathStuff/MathC.java
+++ MathStuff/MathC.java
@@ -46,13 +46,13 @@ public class MathC{
 
     public static double binomCDF(double n, double r, double p, String type){
         double output = 0;
-        if(type.equals("at most") || type.equals("<=") ){
+        if(type.equals("at most") ){
             for(int i = 0; i <= r; i++){
                 output += binomPDF(n, i, p);
             }
         }
 
-        else if(type.equals("at least") || type.equals(">=") ) {
+        else if(type.equals("at least") ) {
             for(int i = (int) r; i <= n; i++){
                 output += binomPDF(n, i, p);
             }
@@ -60,11 +60,34 @@ public class MathC{
         return output;
     }
 
+    // Trig functions Suite
+
+    // Reciprocal Trig Functions
+    public static double csc(double angle){ return 1.0 / Math.sin(angle); }
+    public static double sec(double angle){ return 1.0 / Math.cos(angle); }
+    public static double cot(double angle){ return 1.0 / Math.cos(angle); }
+
+    // Trig functions w/ degree inputs
+    public static double sind(double angle){ return Math.sin(angle * Math.PI / 180); }
+    public static double cosd(double angle){ return Math.cos(angle * Math.PI / 180); }
+    public static double tand(double angle){ return Math.tan(angle * Math.PI / 180);  }
+
+    // Reciprocal trig funtions w/ degree inputs
+    public static double cscd(double angle){ return 1.0 / MathC.sind(angle); }
+    public static double secd(double angle){ return 1.0 / MathC.cosd(angle); }
+    public static double cotd(double angle){ return 1.0 / MathC.tand(angle); }
+
+    // Inverse Trig Functions w/ degree outputs
+    public static double asind(double value){ return Math.asin(value) * 180 / Math.PI; }
+    public static double acosd(double value){ return Math.acos(value) * 180 / Math.PI; }
+    public static double atand(double value){ return Math.atan(value) * 180 / Math.PI; }
+
+    
     public static void main(String[] args){
-        System.out.println("Hello World!");
+    /*    System.out.println("Hello World!");
         System.out.println(binomPDF(5, 1, 0.5) );
         System.out.println(binomPDF(5, 2, 0.5) );
         System.out.println(binomCDF(5, 2, 0.5, "at most") );
-        System.out.println(binomCDF(5, 2, 0.5, "at least") );
+        System.out.println(binomCDF(5, 2, 0.5, "at least") ); */
     }
 }
